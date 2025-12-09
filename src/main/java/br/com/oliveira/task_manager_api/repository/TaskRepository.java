d4698a7e0237c6f6051ce494468fa2f9542e8500
camylla <camylla.oliveiraps@gmail.com>
2025-12-09 09:52:07 -0300

    feat: implement TaskRepository with custom query methods

--- src/main/java/br/com/oliveira/task_manager_api/repository/TaskRepository.java
+++ src/main/java/br/com/oliveira/task_manager_api/repository/TaskRepository.java
@@ -2,10 +2,23 @@ package br.com.oliveira.task_manager_api.repository;
 
 import br.com.oliveira.task_manager_api.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    // Método Busca todas as tarefas onde o user.id é igual ao que passarmos
-    List<Task> findByUserId(Long userId);
+    // Método Busca Tarefas por ID do Dono
+    List<Task> findByOwnerID(Long ownerId);
+
+    // Método Busca Tarefas por ID do Participante
+    List<Task> findByParticipantsID(Long participantId);
+
+    // Método Busca Tarefas onde o Usuário é Dono ou Participante
+    List<Task> findByOwnerIdOrParticipantsId(Long ownerId, Long participantId);
+
+    // Método Busca Tarefas por Data de Vencimento
+    List<Task> findByDueDate(LocalDateTime dueDate);
+
+
 }
